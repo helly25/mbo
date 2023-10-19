6a00665c020516ca41564244aa423811195009b6
helly25 <marcus.borger@snyk.io>
2023-10-19 21:21:31 +0200

    Add more tests.

--- mbo/container/any_scan_test.cc
+++ mbo/container/any_scan_test.cc
@@ -18,20 +18,36 @@
 #include <initializer_list>
 #include <iterator>
 #include <list>
+#include <map>
 #include <set>
+#include <unordered_set>
+#include <utility>
 #include <vector>
 
+#include "absl/hash/hash.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "mbo/container/limited_set.h"
 #include "mbo/container/limited_vector.h"
 #include "mbo/types/traits.h"
 
+namespace std {
+
+template<>
+struct hash<std::pair<std::string, std::string>> {
+  std::size_t operator()(const std::pair<std::string, std::string>& obj) const noexcept {
+    return absl::HashOf(obj.first, obj.second);
+  }
+};
+
+}  // namespace std
+
 namespace mbo::container {
 namespace {
 
 using ::testing::ElementsAre;
 using ::testing::Pair;
+using ::testing::WhenSorted;
 
 static_assert(std::input_iterator<AnyScan<int, std::ptrdiff_t>::const_iterator>);
 static_assert(!std::forward_iterator<AnyScan<int, std::ptrdiff_t>::const_iterator>);
@@ -46,6 +62,11 @@ TEST_F(AnyScanTest, TestArray) {
 
 TEST_F(AnyScanTest, TestInitializerList) {
   std::initializer_list<int> data{1, 2, 3};
+  EXPECT_THAT(MakeAnyScan(data), ElementsAre(1, 2, 3));
+}
+
+TEST_F(AnyScanTest, TestLimitedUnorderedSet) {
+  LimitedSet<int, 3> data{1, 2, 3};
   EXPECT_THAT(MakeAnyScan(data), ElementsAre(1, 2, 3));
 }
 
@@ -69,6 +90,11 @@ TEST_F(AnyScanTest, TestSet) {
   EXPECT_THAT(MakeAnyScan(data), ElementsAre(1, 2, 3));
 }
 
+TEST_F(AnyScanTest, TestUnorderedSet) {
+  std::unordered_set<int> data{1, 2, 3};
+  EXPECT_THAT(MakeAnyScan(data), ElementsAre(1, 2, 3));
+}
+
 TEST_F(AnyScanTest, TestVector) {
   std::vector<int> data{1, 2, 3};
   EXPECT_THAT(MakeAnyScan(data), ElementsAre(1, 2, 3));
@@ -85,6 +111,7 @@ TEST_F(AnyScanTest, CallFunction) {
   EXPECT_THAT(Tester(MakeAnyScan(LimitedVector<int, 3>{1, 2, 3})), ElementsAre(1, 2, 3));
   EXPECT_THAT(Tester(MakeAnyScan(std::list<int>{1, 2, 3})), ElementsAre(1, 2, 3));
   EXPECT_THAT(Tester(MakeAnyScan(std::set<int>{1, 2, 3})), ElementsAre(1, 2, 3));
+  EXPECT_THAT(Tester(MakeAnyScan(std::unordered_set<int>{1, 2, 3})), WhenSorted(ElementsAre(1, 2, 3)));
   EXPECT_THAT(Tester(MakeAnyScan(std::vector<int>{1, 2, 3})), ElementsAre(1, 2, 3));
 }
 
@@ -94,6 +121,7 @@ TEST_F(AnyScanTest, CallFunctionString) {
   EXPECT_THAT(Tester(MakeAnyScan(LimitedVector<std::string, 3>{"a", "b"})), ElementsAre("a", "b"));
   EXPECT_THAT(Tester(MakeAnyScan(std::list<std::string>{"a", "b"})), ElementsAre("a", "b"));
   EXPECT_THAT(Tester(MakeAnyScan(std::set<std::string>{"a", "b"})), ElementsAre("a", "b"));
+  EXPECT_THAT(Tester(MakeAnyScan(std::unordered_set<std::string>{"a", "b"})), WhenSorted(ElementsAre("a", "b")));
   EXPECT_THAT(Tester(MakeAnyScan(std::vector<std::string>{"a", "b"})), ElementsAre("a", "b"));
 }
 
@@ -102,7 +130,9 @@ TEST_F(AnyScanTest, CallFunctionPairOfStrings) {
   EXPECT_THAT(Tester(MakeAnyScan(LimitedSet<std::pair<std::string, std::string>, 3>{{"1", "a"}, {"2", "b"}})), ElementsAre(Pair("1", "a"), Pair("2", "b")));
   EXPECT_THAT(Tester(MakeAnyScan(LimitedVector<std::pair<std::string, std::string>, 3>{{"1", "a"}, {"2", "b"}})), ElementsAre(Pair("1", "a"), Pair("2", "b")));
   EXPECT_THAT(Tester(MakeAnyScan(std::list<std::pair<std::string, std::string>>{{"1", "a"}, {"2", "b"}})), ElementsAre(Pair("1", "a"), Pair("2", "b")));
+  EXPECT_THAT(Tester(MakeAnyScan(std::map<std::string, std::string>{{"1", "a"}, {"2", "b"}})), ElementsAre(Pair("1", "a"), Pair("2", "b")));
   EXPECT_THAT(Tester(MakeAnyScan(std::set<std::pair<std::string, std::string>>{{"1", "a"}, {"2", "b"}})), ElementsAre(Pair("1", "a"), Pair("2", "b")));
+  EXPECT_THAT(Tester(MakeAnyScan(std::unordered_set<std::pair<std::string, std::string>>{{"1", "a"}, {"2", "b"}})), WhenSorted(ElementsAre(Pair("1", "a"), Pair("2", "b"))));
   EXPECT_THAT(Tester(MakeAnyScan(std::vector<std::pair<std::string, std::string>>{{"1", "a"}, {"2", "b"}})), ElementsAre(Pair("1", "a"), Pair("2", "b")));
 }
 
