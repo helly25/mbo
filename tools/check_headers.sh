1c73c8854eb4d7ddd0fd38abb538b7c642493d35
Marcus Boerger <marcus.boerger@gmail.com>
2024-03-25 19:46:52 +0100

    Cleanup (#22)
    * Cleanup: Mostly dropping year from copyright lines.
    
    * No bazel module files.

--- tools/check_headers.sh
+++ tools/check_headers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2023 M. Boerger (helly25.com)
+# Copyright M. Boerger (helly25.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
