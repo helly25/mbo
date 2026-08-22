514e7145bf958283faa15313418ef4e36c99b367
helly25 <6420169+helly25@users.noreply.github.com>
2026-08-22 21:17:43 +0100

    Exclude inaccessible portable load lines

--- mbo/hash/hash_internal_util.h
+++ mbo/hash/hash_internal_util.h
@@ -71,7 +71,7 @@ MBO_FORCE_INLINE constexpr uint64_t Load64(const char* ptr) noexcept {
     }
   }
   uint64_t result = 0;
-  for (std::size_t i = 0; i < 8; ++i) {  // LCOV_EXCL_BR_LINE
+  for (std::size_t i = 0; i < 8; ++i) {  // LCOV_EXCL_LINE
     result |= static_cast<uint64_t>(static_cast<uint8_t>(ptr[i])) << (i * 8U);
   }
   return result;
@@ -131,7 +131,7 @@ MBO_FORCE_INLINE constexpr uint32_t Load32(const char* ptr) noexcept {
     }
   }
   uint32_t result = 0;
-  for (std::size_t i = 0; i < 4; ++i) {  // LCOV_EXCL_BR_LINE
+  for (std::size_t i = 0; i < 4; ++i) {  // LCOV_EXCL_LINE
     result |= static_cast<uint32_t>(static_cast<uint8_t>(ptr[i])) << (i * 8U);
   }
   return result;
